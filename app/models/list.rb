ff975dc05c5e13daee0b9279ad8b3a14f1fc0e02
anthonymicho <anthony.micho15@gmail.com>
2025-05-23 15:48:20 +0200

    Exo set

---
+++ app/models/list.rb
@@ -0,0 +1,6 @@
+class List < ApplicationRecord
+  has_one_attached :image
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+  validates :name, uniqueness: true, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "json"
+require "open-uri"
+require 'faker'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.parse(url).read
+movies = JSON.parse(movies_serialized)
+
+movies["results"].each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+
+10.times do
+  List.create!(
+    name: Faker::Book.unique.genre
+  )
+end
+
+5.times do
+  Bookmark.create!(
+    comment: Faker::Quote.famous_last_words,
+    list: List.all.sample,
+    movie: Movie.all.sample
+  )
+end
